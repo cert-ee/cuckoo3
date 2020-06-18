866df972ee324c80af287e9710c6b77964b30cfb
Ricardo van Zutphen <ricardo@hatching.io>
2020-06-18 19:24:48 +0200

    Add cuckoo-common dep to machineries package

--- machineries/setup.py
+++ machineries/setup.py
@@ -11,15 +11,13 @@ if sys.version[0] == "2":
         "The latest version of Cuckoo is Python >=3.6 only. Any Cuckoo version "
         "earlier than 3.0.0 supports Python 2."
     )
-x = setuptools.find_namespace_packages(include=["cuckoo.*"])
 
-print(f"X-> {x}")
 setuptools.setup(
     name="Cuckoo-machineries",
     version="0.1.0",
     author="Stichting Cuckoo Foundation",
     author_email="cuckoo@cuckoofoundation.org",
-    packages=x,
+    packages=setuptools.find_namespace_packages(include=["cuckoo.*"]),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Information Technology",
@@ -37,5 +35,8 @@ setuptools.setup(
     license="GPLv3",
     description="Cuckoo Sandbox machinery modules and helpers",
     zip_safe=False,
-    include_package_data=True
+    include_package_data=True,
+    install_requires=[
+        "Cuckoo-common==0.1.0",
+    ],
 )
