59a22a8071bc774245cba73c604b513ff0a4a719
Ricardo van Zutphen <ricardo@hatching.io>
2020-08-27 17:42:05 +0200

    Add url and static global functions to use in Jinja templates

--- web/cuckoo/web/web/jinja/env.py
+++ web/cuckoo/web/web/jinja/env.py
@@ -2,11 +2,19 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
 from jinja2.environment import Environment
 
 from .filters import filters
 
+_globals = {
+    "static": staticfiles_storage.url,
+    "url": reverse
+}
+
 def environment(**kwargs):
     env = Environment(**kwargs)
     env.filters.update(filters)
+    env.globals.update(_globals)
     return env
