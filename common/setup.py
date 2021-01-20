8dd7f70ed804c797f88d73e09bc9b8bf0c03e27f
Ricardo van Zutphen <ricardo@hatching.io>
2021-01-20 23:44:08 +0100

    Add misp helper client

--- common/setup.py
+++ common/setup.py
@@ -45,6 +45,7 @@ setuptools.setup(
         "sqlalchemy>=1.3.13, <1.4",
         "elasticsearch>=7.8.1, <8.0",
         "elasticsearch-dsl>=7.2.1, <7.3",
-        "vt-py>=0.5.4, <0.6"
+        "vt-py>=0.5.4, <0.6",
+        "pymisp>=2.4.135.3, <2.5"
     ]
 )
