e9103914f8b8fe28b20ab4b8245dbaf7f3c2c865
Ricardo van Zutphen <ricardo@hatching.io>
2021-01-21 00:05:58 +0100

    Add iso datetime formatting filter

--- web/cuckoo/web/web/jinja/filters.py
+++ web/cuckoo/web/web/jinja/filters.py
@@ -2,15 +2,30 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+from datetime import datetime
+
 from cuckoo.common.analyses import States
 
 def do_formatdatetime(value, fmt="%Y-%m-%d %H:%M"):
     return value.strftime(fmt)
+
+def do_formatisodatetime(value, fmt="%Y-%m-%d %H:%M"):
+    millis_index = value.find(".")
+
+    # Ignore the milliseconds. They would cause a value error and might
+    # not always be present.
+    if millis_index:
+        value = value[:millis_index]
+
+    return do_formatdatetime(
+        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S"), fmt
+    )
 
 def do_humanstate(value):
     return States.to_human(value)
 
 filters = {
     "formatdatetime": do_formatdatetime,
+    "formatisodatetime": do_formatisodatetime,
     "humanstate": do_humanstate
 }
