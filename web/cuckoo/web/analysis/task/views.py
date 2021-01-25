55704699aa9cfdc552558d22302d6fc1e89300fd
Ricardo van Zutphen <ricardo@hatching.io>
2021-01-25 20:15:00 +0100

    Add task human state filter and render error page

--- web/cuckoo/web/analysis/task/views.py
+++ web/cuckoo/web/analysis/task/views.py
@@ -5,6 +5,7 @@
 from django.http import HttpResponseServerError, HttpResponseNotFound
 from django.shortcuts import render
 
+from cuckoo.common.task import States
 from cuckoo.common.storage import TaskPaths, AnalysisPaths
 from cuckoo.common.machines import Machine
 from cuckoo.common.strictcontainer import Analysis, Task, Post
@@ -13,6 +14,22 @@ def index(request, analysis_id, task_id):
     try:
         analysis = Analysis.from_file(AnalysisPaths.analysisjson(analysis_id))
         task = Task.from_file(TaskPaths.taskjson(task_id))
+    except FileNotFoundError:
+        return HttpResponseNotFound()
+    except (ValueError, KeyError, TypeError) as e:
+        return HttpResponseServerError(str(e))
+
+    if task.state == States.FATAL_ERROR:
+        return render(
+            request, template_name="task/error.html.jinja2",
+            context={
+                "analysis": analysis.to_dict(),
+                "task": task.to_dict(),
+                "analysis_id": analysis_id
+            }
+        )
+
+    try:
         postreport = Post.from_file(TaskPaths.report(task_id))
         machine = Machine.from_file(TaskPaths.machinejson(task_id))
     except FileNotFoundError:
