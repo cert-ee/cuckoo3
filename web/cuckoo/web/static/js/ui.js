d0c773ac5fce9b3fa28bb90d63518bf1d18b1610
Koen Houtman <automagisch@hatching.io>
2021-02-01 11:01:20 +0100

    Polar>Radar, A funky color set for a fancy chart

--- web/cuckoo/web/static/js/ui.js
+++ web/cuckoo/web/static/js/ui.js
@@ -58,6 +58,15 @@ window.lib = Object.assign(window.lib || {}, {
         <p class="column no-margin-y">${content}</p>
       </div>
     `);
+  },
+  // array shuffling function
+  // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+  shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
   }
 });
 
