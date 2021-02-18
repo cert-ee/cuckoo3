83f79c2fa0a16e3c52d14a4afeeab5aec71c5e48
Ricardo van Zutphen <ricardo@hatching.io>
2021-02-18 17:37:31 +0100

    Don't finalize patterns scans if no scanner was available

--- processing/cuckoo/processing/post/eventconsumer/patternsigs.py
+++ processing/cuckoo/processing/post/eventconsumer/patternsigs.py
@@ -88,7 +88,7 @@ class PatternFinder(EventConsumer):
         event.pattern_scan(self.scanner)
 
     def finalize(self):
-        if not self.match_tracker:
+        if not self.scanner:
             return
 
         matched_patternsigs = self.match_tracker.get_matches()
