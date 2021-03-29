8b76130932d0fafdf269775223babc5c3bf13840
Ricardo van Zutphen <ricardo@hatching.io>
2021-03-29 14:47:56 +0200

    Exportable task work and uniformalize zip code

--- common/cuckoo/common/importing.py
+++ common/cuckoo/common/importing.py
@@ -4,17 +4,18 @@
 
 import os
 import stat
-from threading import RLock
 import uuid
 import zipfile
+from pathlib import Path
+from threading import RLock
 
 from . import analyses
 from .clients import ImportControllerClient, ActionFailedError
 from .storage import (
-    AnalysisPaths, split_analysis_id, create_analysis_folder, Binaries, File,
-    Paths, move_file, delete_file
+    AnalysisPaths, TaskPaths, split_analysis_id, create_analysis_folder,
+    Binaries, File, Paths, move_file, delete_file, TASK_PREFIX, taskdir_name
 )
-from .strictcontainer import Analysis
+from .strictcontainer import Analysis, Task
 
 class AnalysisImportError(Exception):
     pass
@@ -25,7 +26,7 @@ class AnalysisExistsError(Exception):
 class AnalysisZippingError(AnalysisImportError):
     pass
 
-def read_analysisjson(zipped_analysis, passwordbytes=None):
+def _read_analysisjson(zipped_analysis, passwordbytes=None):
     zip_file = zipped_analysis.zip_fp
     if passwordbytes:
         zip_file.setpassword(passwordbytes)
@@ -48,137 +49,91 @@ def read_analysisjson(zipped_analysis, passwordbytes=None):
     except (ValueError, KeyError, TypeError) as e:
         raise AnalysisImportError(f"Invalid analysis.json: {e}")
 
-def unzip_analysis(zipped_analysis, unzip_path, passwordbytes=None):
-    if not os.path.isdir(unzip_path):
+def _read_taskjson(zipped_analysis, task_id, passwordbytes=None):
+    zip_file = zipped_analysis.zip_fp
+    if passwordbytes:
+        zip_file.setpassword(passwordbytes)
+
+    taskdir = taskdir_name(task_id)
+    try:
+        taskfile = zip_file.getinfo(f"{taskdir}/task.json")
+    except KeyError:
         raise AnalysisImportError(
-            f"Unzip path is not a directory: {unzip_path}"
+            "Invalid analysis zip. Missing file task.json"
         )
 
-    zip_file = zipped_analysis.zip_fp
+    # Ignore task.json if larger than 50MB. It should never be
+    # that big. It is a relatively small JSON structure. We are not
+    # even going to try and parse something that large.
+    if taskfile.file_size > 50 * 1024 * 1024:
+        raise AnalysisImportError("task.json exceeds 50MB")
 
-    illegal = ("..", ":", "\x00")
+    try:
+        return Task.from_string(zip_file.read(f"{taskdir}/task.json"))
+    except (ValueError, KeyError, TypeError) as e:
+        raise AnalysisImportError(f"Invalid task.json: {e}")
+
+_ILLEGAL_CHARS = ("..", ":", "\x00")
+
+def _has_illegal_chars(zipinfo):
+    name = zipinfo.filename
+    return any(c in name for c in _ILLEGAL_CHARS) or name.startswith("/")
+
+def _should_ignore(zipinfo):
+    info = zipinfo.external_attr >> 16
+    # Ignore everything that is not a regular file or directory.
+    # We don't zip them, so we also don't unzip them.
+    # Python3 zipfile does not (currently) support unpacking symlinks,
+    # but there are requests and code reviews for it.
+    if not stat.S_ISREG(info) and not stat.S_ISDIR(info):
+        return True
+
+    # To prevent zip bombs, skip files with a high compression ratio.
+    if zipinfo.compress_size > 0:
+        if zipinfo.file_size / zipinfo.compress_size > 1500:
+            return True
+
+    return False
+
+def _get_unzippables(zipped_data, passwordbytes=None):
+    zip_file = zipped_data.zip_fp
+
     unzippables = []
     if passwordbytes:
         zip_file.setpassword(passwordbytes)
 
     for file in zip_file.filelist:
-        name = file.filename
-        if any(c in name for c in illegal) or name.startswith("/"):
+
+        if _has_illegal_chars(file):
             raise AnalysisImportError(
-                f"Illegal characters in path of file: {name}"
+                f"Illegal characters in path of file: {file.filename}"
             )
 
-        info = file.external_attr >> 16
-        # Ignore everything that is not a regular file or directory.
-        # We don't zip them, so we also don't unzip them.
-        # Python3 zipfile does not (currently) support unpacking symlinks,
-        # but there are requests and code reviews for it.
-        if not stat.S_ISREG(info) and not stat.S_ISDIR(info):
-            continue
-
-        # To prevent zip bombs, skip files with a high compression ratio.
-        if file.file_size / file.compress_size > 1500:
+        if _should_ignore(file):
             continue
 
         unzippables.append(file.filename)
 
+    return unzippables
+
+def _unzip_zipped_data(zipped_data, unzip_path, passwordbytes=None):
+    if not os.path.isdir(unzip_path):
+        raise AnalysisImportError(
+            f"Unzip path is not a directory: {unzip_path}"
+        )
+
+    unzippables = _get_unzippables(zipped_data, passwordbytes=passwordbytes)
     if not unzippables:
         raise AnalysisImportError("Nothing to unzip after filtering")
 
-    zip_file.extractall(path=unzip_path, members=unzippables)
+    zipped_data.zip_fp.extractall(path=unzip_path, members=unzippables)
 
-class AnalysisZipper:
-
-    def __init__(self, analysis_id):
-        self.id = analysis_id
-        self._rootfiles = []
-        self._tasks = {}
-        self._check_valid()
-        self._discover()
-
-    def _check_valid(self):
-        try:
-            Analysis.from_file(AnalysisPaths.analysisjson(self.id))
-        except (FileNotFoundError, KeyError, ValueError, TypeError) as e:
-            raise AnalysisZippingError(
-                f"Invalid analysis JSON. Cannot zip analysis: {e}"
-            )
-
-    def make_zip(self, zip_path, ignore_task_dirs=[], ignore_task_files=[]):
-        if os.path.exists(zip_path):
-            raise AnalysisZippingError("Zip path already exists")
-
-        zippables = []
-        zippables.extend(self._rootfiles)
-        zippables.extend(
-            self._make_task_zippables(ignore_task_dirs, ignore_task_files)
-        )
-
-        with zipfile.ZipFile(zip_path, "x", compression=zipfile.ZIP_STORED,
-                             allowZip64=True) as zipf:
-            for zippable in zippables:
-                fullpath, arcname = zippable
-                if not fullpath:
-                    # An empty dir, add it.
-                    if arcname.endswith("/"):
-                        zipf.writestr(zipfile.ZipInfo(filename=arcname), "")
-                else:
-                    zipf.write(fullpath, arcname=arcname)
-
-        return ZippedAnalysis(zip_path)
-
-    def _make_task_zippables(self, ignore_dirs=[], ignore_files=[]):
-        zippables = []
-        for task_name, entries in self._tasks.items():
-            for arcname, fullpath in entries.items():
-                if arcname.startswith(tuple(ignore_dirs) + tuple(ignore_files)):
-                    continue
-
-                zippables.append((fullpath, f"{task_name}/{arcname}"))
-
-            # Create the empty dirs
-            for dirname in ignore_dirs:
-                zippables.append((None, f"{task_name}/{dirname}/"))
-
-        return zippables
-
-    def _add_task(self, task_name, path):
-        task = self._tasks.setdefault(task_name, {})
-        for curpath, dirs, files in os.walk(path, followlinks=False):
-            for name in files:
-                fullpath = os.path.join(curpath, name)
-                arcpath = os.path.relpath(fullpath, path)
-                task[arcpath] = fullpath
-
-    def _add_dir(self, path, analysis_path):
-        for curpath, dirs, files in os.walk(path, followlinks=False):
-            for name in files:
-                fullpath = os.path.join(curpath, name)
-                self._rootfiles.append(
-                    (fullpath, os.path.relpath(fullpath, analysis_path))
-                )
-
-    def _discover(self):
-        analysis_path = AnalysisPaths.path(self.id)
-        for name in os.listdir(analysis_path):
-            fullpath = os.path.join(analysis_path, name)
-            if os.path.isdir(fullpath):
-                if name.startswith("task_"):
-                    self._add_task(name, fullpath)
-                else:
-                    self._add_dir(fullpath, analysis_path)
-            else:
-                self._rootfiles.append(
-                    (fullpath, os.path.relpath(fullpath, analysis_path))
-                )
-
-class ZippedAnalysis:
+class ZippedData:
 
     def __init__(self, zipfile_path):
         self._path = zipfile_path
         self._fp = None
         self._zfp = None
-        self._analysis = None
         self._lock = RLock()
 
     @property
@@ -199,15 +154,8 @@ class ZippedAnalysis:
 
         return self._zfp
 
-    @property
-    def analysis(self):
-        if not self._analysis:
-            self._analysis = read_analysisjson(self)
-
-        return self._analysis
-
     def unzip(self, unpack_path):
-        unzip_analysis(self, unpack_path)
+        _unzip_zipped_data(self, unpack_path)
 
     def open_zipfile(self):
         with self._lock:
@@ -248,8 +196,301 @@ class ZippedAnalysis:
         if self._zfp:
             self.close_zipfile()
 
+
+class ZippedAnalysis(ZippedData):
+
+    def __init__(self, zipfile_path):
+        super().__init__(zipfile_path)
+        self._analysis = None
+
+    @property
+    def analysis(self):
+        if not self._analysis:
+            self._analysis = _read_analysisjson(self)
+
+        return self._analysis
+
+class ZippedNodeWork(ZippedAnalysis):
+
+    TASK_ID_FILE = "nodework"
+
+    def __init__(self, zipfile_path):
+        super().__init__(zipfile_path)
+        self._task_id = None
+        self._task_name = None
+        self._task = None
+
+    @property
+    def task_id(self):
+        if not self._task_id:
+            self._find_task_id()
+
+        return self._task_id
+
+    @property
+    def taskdir_name(self):
+        if not self._task_name:
+            self._find_task_id()
+
+        return self._task_name
+
+    def _find_task_id(self):
+        nodework_info = self.zip_fp.getinfo(self.TASK_ID_FILE)
+        if not nodework_info:
+            raise AnalysisImportError(
+                f"Missing {self.TASK_ID_FILE} in node work zip"
+            )
+
+        if _should_ignore(nodework_info):
+            raise AnalysisImportError(
+                f"{self.TASK_ID_FILE} is not a regular file"
+            )
+
+        # This file only contains a task ID, and should therefore never be
+        # larger than date + analysis id length + _XXX
+        if nodework_info.file_size > 30:
+            raise AnalysisImportError(
+                f"{self.TASK_ID_FILE} file exceeds maximum size"
+            )
+
+        task_id = self.zip_fp.read(nodework_info).decode()
+        try:
+            self._task_name = taskdir_name(task_id)
+        except ValueError as e:
+            raise AnalysisImportError(e)
+
+        self._task_id = task_id
+
+    @property
+    def task(self):
+        if not self._task:
+            self._task = _read_taskjson(self, self.task_id)
+
+        return self._task
+
+    def unzip(self, unpack_path, task_only=False):
+        """Unzip the current zip to the unpack path. If task_only is
+        true, the task_id will be used to search for a matching directory
+        name in the zip, and only unpack that."""
+        unpack_path = Path(unpack_path)
+        taskdir = unpack_path.joinpath(self.taskdir_name)
+        if taskdir.exists():
+            raise AnalysisImportError(f"Task path {taskdir} already exists")
+
+        all_unzippables = _get_unzippables(self)
+
+        # Remove the task id file. We never want to actually unpack it
+        # to disk.
+        all_unzippables.remove(self.TASK_ID_FILE)
+
+        if task_only:
+            # The analysis directory already exists. Filter out all
+            # files that are not in the task_name directory.
+            for unzippable in all_unzippables[:]:
+                if not unzippable.startswith(self.taskdir_name):
+                    all_unzippables.remove(unzippable)
+
+        self.zip_fp.extractall(path=unpack_path, members=all_unzippables)
+
+class Zipper:
+
+    WRAPPER_CLASS = ZippedData
+
+    @property
+    def archive_root(self):
+        raise NotImplementedError
+
+    def _get_all_zippables(self):
+        """Must return a list of (fullpath, archive path) tuples that should
+        be added to the zip"""
+        raise NotImplementedError
+
+    def _make_zippable_dir(self, dirpath):
+        relpath = os.path.relpath(dirpath, self.archive_root)
+        if not relpath.endswith("/"):
+            relpath += "/"
+
+        info = zipfile.ZipInfo(filename=relpath)
+        # Set the external attribute such that is represents a directory
+        info.external_attr = stat.S_IFDIR << 16
+        return (None, info, None)
+
+    def _make_zippable_file(self, filepath):
+        return (filepath,
+                zipfile.ZipInfo.from_file(
+                    filename=filepath,
+                    arcname=os.path.relpath(filepath, self.archive_root)
+                ),
+                None)
+
+    def _make_zippable_data(self, archive_path, data):
+        info = zipfile.ZipInfo(filename=archive_path)
+        info.external_attr = stat.S_IFREG << 16
+        return (None, info, data)
+
+    def make_zip(self, zip_path):
+        if os.path.exists(zip_path):
+            raise AnalysisZippingError(f"Path already exists: {zip_path}")
+
+        with zipfile.ZipFile(zip_path, "x", compression=zipfile.ZIP_STORED,
+                             allowZip64=True) as zipf:
+            for fullpath, zipinfo, data in self._get_all_zippables():
+                if zipinfo.is_dir():
+                    zipf.writestr(zipinfo, "")
+                elif data:
+                    zipf.writestr(zipinfo, data)
+                else:
+                    zipf.write(fullpath, arcname=zipinfo.filename)
+
+        return self.WRAPPER_CLASS(zip_path)
+
+
+class TaskZipper(Zipper):
+
+    def __init__(self, analysis_id, task_id):
+        self.analysis_id = analysis_id
+        self.task_id = task_id
+
+        self._arcroot = None
+
+    @property
+    def archive_root(self):
+        if not self._arcroot:
+            self._arcroot = AnalysisPaths.path(self.analysis_id)
+
+        return self._arcroot
+
+    def _get_task_zippables(self):
+        task_path = TaskPaths.path(self.task_id)
+        zippables = []
+        for curpath, dirs, files in os.walk(task_path, followlinks=False):
+            for dirname in dirs:
+                # Always add all directories, even if empty.
+                zippables.append(
+                    self._make_zippable_dir(os.path.join(curpath, dirname))
+                )
+
+            for name in files:
+                zippables.append(
+                    self._make_zippable_file(os.path.join(curpath, name))
+                )
+
+        return zippables
+
+    def _get_all_zippables(self):
+        return self._get_task_zippables()
+
+
+class NodeWorkZipper(TaskZipper):
+
+    WRAPPER_CLASS = ZippedNodeWork
+
+    def _get_all_zippables(self):
+        zippables = []
+        zippables.extend(self._get_task_zippables())
+        zippables.append(
+            self._make_zippable_file(
+                AnalysisPaths.analysisjson(self.analysis_id)
+            )
+        )
+
+        targetzip = AnalysisPaths.zipified_file(self.analysis_id)
+        if targetzip.is_file():
+            zippables.append(
+                self._make_zippable_file(targetzip)
+            )
+        else:
+            # Set resolve to False, because the binary path is a symlink
+            # and we want a relative path to the actual symlink in the
+            # analysis directory, not its value.
+            zippables.append(
+                self._make_zippable_file(
+                    AnalysisPaths.submitted_file(self.analysis_id,
+                                                 resolve=False)
+                )
+            )
+
+        zippables.append(
+            self._make_zippable_data(ZippedNodeWork.TASK_ID_FILE, self.task_id)
+        )
+
+        return zippables
+
+
+class AnalysisZipper(Zipper):
+
+    WRAPPER_CLASS = ZippedAnalysis
+
+    def __init__(self, analysis_id, ignore_dirs=[], ignore_files=[]):
+        self.id = analysis_id
+        self.ignore_files = tuple(ignore_files)
+        self.ignore_dirs = tuple(ignore_dirs)
+
+        self._arcroot = None
+        self._check_valid()
+
+    @property
+    def archive_root(self):
+        if not self._arcroot:
+            self._arcroot = AnalysisPaths.path(self.id)
+
+        return self._arcroot
+
+    def _check_valid(self):
+        try:
+            Analysis.from_file(AnalysisPaths.analysisjson(self.id))
+        except (FileNotFoundError, KeyError, ValueError, TypeError) as e:
+            raise AnalysisZippingError(
+                f"Invalid analysis JSON. Cannot zip analysis: {e}"
+            )
+
+    def _add_task(self, task_path):
+        zippables = []
+        for curpath, dirs, files in os.walk(task_path, followlinks=False):
+            for dirname in dirs:
+                # Always add all directories, even if empty.
+                zippables.append(
+                    self._make_zippable_dir(os.path.join(curpath, dirname))
+                )
+
+            for name in files:
+                fullpath = os.path.join(curpath, name)
+                relpath = os.path.relpath(fullpath, task_path)
+
+                # Only add the file if its relative path does not appear
+                # in the ignored file or directory list.
+                if not relpath.startswith(
+                        self.ignore_dirs + self.ignore_files
+                ):
+                    zippables.append(self._make_zippable_file(fullpath))
+
+        return zippables
+
+    def _add_dir(self, path):
+        zippables = []
+        for curpath, dirs, files in os.walk(path, followlinks=False):
+            for name in files:
+                fullpath = os.path.join(curpath, name)
+                zippables.append(self._make_zippable_file(fullpath))
+
+        return zippables
+
+    def _get_all_zippables(self):
+        zippables = []
+        for fullpath in self.archive_root.iterdir():
+            if fullpath.is_dir():
+                if fullpath.name.startswith(TASK_PREFIX):
+                    zippables.extend(self._add_task(fullpath))
+                else:
+                    zippables.extend(self._add_dir(fullpath))
+            else:
+                zippables.append(self._make_zippable_file(fullpath))
+
+        return zippables
+
+
 def _import_targetfile(analysis):
-    tmpbin = os.path.join(AnalysisPaths.path(analysis.id), f".{uuid.uuid4()}")
+    tmpbin = AnalysisPaths.path(analysis.id).joinpath(f".{uuid.uuid4()}")
     binary = AnalysisPaths.submitted_file(analysis.id)
 
     try:
@@ -264,7 +505,7 @@ def _import_targetfile(analysis):
 
     os.rename(binary, tmpbin)
     binary_helper.symlink(binary)
-    os.remove(tmpbin)
+    delete_file(tmpbin)
 
 def import_analysis(analysis_zip_path, delete_after_import=False):
     if not analysis_zip_path.endswith(".zip"):
