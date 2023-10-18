472eeddac38065d05ceab6601b4909bcded6ff97
Ants <ants@cert.ee>
2023-10-18 11:09:50 +0300

    Not needed anymore because pycrypto removed from roach

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Setuptools >58.0.0 removed the build_py_2to3 function that is used in pycrypto's setup.py.
-pip install Setuptools==58.0.0
+pip install -U wheel
+pip install -U requests
 # TMP solution until new versions of sflock etc are released to PyPI
 pip install -U git+https://github.com/cert-ee/peepdf
 pip install -U git+https://github.com/cert-ee/sflock
 pip install -U git+https://github.com/cert-ee/roach
 pip install -U git+https://github.com/cert-ee/httpreplay
-
-pip install -U requests
-pip install -U wheel
 
 declare -a pkglist=("./common" "./processing" "./machineries" "./web" "./node" "./core")
 
